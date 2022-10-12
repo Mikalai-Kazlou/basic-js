17b34656ddc4fd715761029aa1187b446747e19f
Mikalai Kazlou <mikalai_kazlou@epam.com>
2022-10-12 15:25:33 +0300

    feat: add "Common character count", "Delete digit"

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function deleteDigit(n) {
+  const s = n.toString();
+  let max = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let part = s.slice(0, i) + s.slice(i + 1);
+    max = (max < Number(part)) ? Number(part) : max;
+  }
+
+  return max;
 }
 
 module.exports = {
