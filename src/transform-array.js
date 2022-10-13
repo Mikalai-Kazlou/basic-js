6b6800fef93cafd2396af7fbf67774defc1f3ef9
Mikalai Kazlou <mikalai_kazlou@epam.com>
2022-10-13 19:16:36 +0300

    feat: add "Transform array"

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+
+  const result = [];
+  const cs = {
+    dcn: '--discard-next',
+    dcp: '--discard-prev',
+    dbn: '--double-next',
+    dbp: '--double-prev'
+  };
+
+  function addValue(prev, curr, next) {
+    result.push(curr);
+
+    if (typeof (curr) !== 'number') {
+      return;
+    }
+
+    if (prev === cs.dcn) {
+      result.pop();
+      return;
+    }
+    if (prev === cs.dbn) {
+      result.push(curr);
+    }
+    if (next === cs.dcp) {
+      result.pop();
+      return;
+    }
+    if (next === cs.dbp) {
+      result.push(curr);
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!Object.values(cs).includes(arr[i])) {
+      addValue(arr[i - 1], arr[i], arr[i + 1]);
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
