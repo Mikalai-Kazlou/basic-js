3d9605bd465e4d1e8bf24f8eff1a28e8c99056c4
Mikalai Kazlou <mikalai_kazlou@epam.com>
2022-10-14 15:33:43 +0300

    feat: add "Vigenere cipher"

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,81 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.reverse = !direct;
+
+    this.letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    this.square = this.generateSquare(this.letters);
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  generateSquare(letters) {
+    const square = [];
+    for (let i = 0; i < letters.length; i++) {
+      square[i] = letters.slice(i).concat(letters.slice(0, i));
+    }
+    return square;
+  }
+
+  encrypt(txt, key) {
+    if (!txt || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    txt = txt.toUpperCase();
+    key = key.toUpperCase();
+
+    let str = '';
+    for (let i = 0, j = 0; i < txt.length; i++) {
+      if (this.letters.includes(txt[i])) {
+        j = (j + key.length) % key.length;
+
+        const row = this.letters.indexOf(key[j]);
+        const col = this.letters.indexOf(txt[i]);
+
+        str += this.square[col][row];
+
+        j++;
+      } else {
+        str += txt[i];
+      }
+    }
+
+    if (this.reverse) {
+      return str.split('').reverse().join('');
+    } else {
+      return str;
+    }
+  }
+
+  decrypt(txt, key) {
+    if (!txt || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    txt = txt.toUpperCase();
+    key = key.toUpperCase();
+
+    let str = '';
+    for (let i = 0, j = 0; i < txt.length; i++) {
+      if (this.letters.includes(txt[i])) {
+        j = (j + key.length) % key.length;
+
+        const row = this.letters.indexOf(key[j]);
+        const col = this.square[row].indexOf(txt[i]);
+
+        str += this.letters[col];
+
+        j++;
+      } else {
+        str += txt[i];
+      }
+    }
+
+    if (this.reverse) {
+      return str.split('').reverse().join('');
+    } else {
+      return str;
+    }
   }
 }
 
